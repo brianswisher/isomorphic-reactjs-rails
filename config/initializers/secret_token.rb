98b85d5709f64087f06363ac297d32011ade5e69
brianswisher <bswisher@gmail.com>
2015-03-03 14:27:05 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IsomorphicReactjs::Application.config.secret_key_base = '430d110dd501ee05d581e3550279419072da5e8c863d122900bd4e980140b1f02f8d0d5974646cfffe78ebe46d671d7290ea5fbd827fa8708b61e9826c4aa24b'
